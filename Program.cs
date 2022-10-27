67f4c8c6395ee972946ee917dfeae9d0627502b1
Eduardo Jaime <eduardojaime86@gmail.com>
2022-10-27 18:42:25 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace COMP1030ProgrammingFundamentals_TestShell;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 01) Solution for problem 1
+
+        // -------------------------------
+        // 02) Solution for problem 2
+
+        // -------------------------------
+        // 03) Solution for problem 3
+
+        // -------------------------------
+        // 04) Solution for problem 4
+
+        // -------------------------------
+        // 05) Solution for problem 5
+
+        // -------------------------------
+        // 06) Solution for problem 6
+
+        // -------------------------------
+        // 07) Solution for problem 7
+
+        // -------------------------------
+        // 08) Solution for problem 8
+
+        // -------------------------------
+        // 09) Solution for problem 9
+
+        // -------------------------------
+        // 10) Solution for problem 10
+
+        // -------------------------------
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# COMP1030ProgrammingFundamentals-TestShell
- 
+# COMP1030 Programming Fundamentals Test Shell
+
+## Instructions
+
+- Download this project and open with Visual Studio Code
+- Open Program.cs
+- Provide a solution for each problem in the test in the section of the file specified with a comment
+- Make sure to format your code and remove unused white spaces
+- Test your program using dotnet cli:
+` dotnet run `
+- Program must run without any errors
+- Make your project folder into a Zip file
+- Submit your code via Blackboard
